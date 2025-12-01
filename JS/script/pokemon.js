1dc632bdff4bf412c13de80a4224a2cee28b29c6
Vinx92 <vv92@tiscali.it>
2025-12-01 21:47:06 +0100

    sistemazione api

--- JS/script/pokemon.js
+++ JS/script/pokemon.js
@@ -10,7 +10,23 @@ import {
 const BOX_ICONA_PROFILO = document.getElementById("box-icona-profilo");
 const CONT_CARD_POKEMON = document.getElementById("cont-card-pokemon");
 const CONT_CERCA_TIPO = document.getElementById("cont-cerca-tipo");
+let pokemon = await pokemonGenerazione(
+  GENERAZIONI[0].inizio,
+  GENERAZIONI[0].fine
+);
+const TAB_GENERAZIONI = document.getElementById("tab-generazioni")
+
+TAB_GENERAZIONI.addEventListener("click", async (e)=>{
+  CONT_CARD_POKEMON.innerHTML = ""
+  pokemon = await pokemonGenerazione(
+  GENERAZIONI[1].inizio,
+  GENERAZIONI[1].fine
+);
+creaCard(CONT_CARD_POKEMON, pokemon)
+})
+
 BOX_ICONA_PROFILO.addEventListener("click", OpenTab);
 
 generaCercaTipo(CONT_CERCA_TIPO, TIPI_POKEMON);
-creaCard(CONT_CARD_POKEMON, await pokemonGenerazione(GENERAZIONI[0].inizio, GENERAZIONI[0].fine));
+
+creaCard(CONT_CARD_POKEMON, pokemon);
